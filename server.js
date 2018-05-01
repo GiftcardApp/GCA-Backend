3c520d9d8c4515d0b02fb0a60c5cd10b0854bcd4
DavidGWheeler <d.wheeler1@comcast.net>
2018-04-30 20:48:37 -0700

    add some tests for user auth to set up for TDD user auth creation

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+require('dotenv').load()
+
+const express = require('express')
+const cors = require('cors')
+const Promise = require('bluebird')
+// const authRoutes = require('./routes/auth-routes')
+// TODO: find out what routes I need to add here.
+const bodyParser = require('body-parser').json()
+const mongoose = require('mongoose')
+
+const app = module.exports = express()
+const router = express.Router()
+const PORT = process.env.PORT || 3000
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/gca-backend-dev'
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URI)
+
+app.use(cors())
+app.use(bodyParser)
+// app.use('/api', authRoutes(router))
+// TODO: and here...
+
+app.listen(PORT, () => console.log('Listening on ${PORT}'));
